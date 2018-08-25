12cd47201bf219850b3f9d83895baad6af08bb4e
eldarmoore <eldar.moore@gmail.com>
2018-08-25 23:37:31 +0300

    New Patch - Doctrine 1. Custom Query Class created, for more visit "http://bit.ly/pro-doctrine"

--- src/AppBundle/Entity/Blog.php
+++ src/AppBundle/Entity/Blog.php
@@ -11,7 +11,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BlogRepository")
  * @ORM\Table(name="blog")
  */
 class Blog
---
+++ src/AppBundle/Repository/BlogRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eldarmoore
+ * Date: 25/08/2018
+ * Time: 23:25
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class BlogRepository extends EntityRepository
+{
+    // Custom Query function "http://bit.ly/pro-doctrine"
+}
