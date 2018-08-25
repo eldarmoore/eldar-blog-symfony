7654afbd194837f94bec57e1edc83f05ed134adf
eldarmoore <eldar.moore@gmail.com>
2018-08-25 22:13:10 +0300

    New Patch - Doctrine 1. Tables modified for the Blog 2. Migrations bundle included 3. Rout for creating blogs created

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -9,15 +9,34 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Blog;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
+    public $content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci alias asperiores corporis delectus doloremque dolores, enim facilis fugit id incidunt, libero, maxime nam nulla perspiciatis quidem quos similique sunt.';
+
+    public function newAction()
+    {
+        $blog = new Blog();
+        $blog->setTitle('My first Title');
+        $blog->setArticle($this->content);
+        $blog->setBlogDate(new \DateTime());
+        $blog->setUserId(rand(10, 9999));
+
+        // Entity Manager
+        // Saving Data
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blog);
+        $em->flush();
+
+        return new Response('<html><body>Blog created!</body></html>');
+    }
 
     public function indexAction($blogId)
     {
-
-        $content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci alias asperiores corporis delectus doloremque dolores, enim facilis fugit id incidunt, libero, maxime nam nulla perspiciatis quidem quos similique sunt.';
 
         return $this->render('default/blog_page.html.twig', array(
             'id'    => $blogId,
@@ -25,4 +44,5 @@ class BlogController extends Controller
             'content' => $content
         ));
     }
+
 }
--- src/AppBundle/Entity/Blog.php
+++ src/AppBundle/Entity/Blog.php
@@ -42,4 +42,70 @@ class Blog
      * @ORM\Column(type="integer")
      */
     private $user_id;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArticle()
+    {
+        return $this->article;
+    }
+
+    /**
+     * @param mixed $article
+     */
+    public function setArticle($article)
+    {
+        $this->article = $article;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBlogDate()
+    {
+        return $this->blog_date;
+    }
+
+    /**
+     * @param mixed $blog_date
+     */
+    public function setBlogDate($blog_date)
+    {
+        $this->blog_date = $blog_date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+
 }
